1d87c4f6b41139fb51d706ca08725d83845aba7d
nusrath432 <nusrath432@users.noreply.github.com>
2023-05-29 02:35:48 +0100

    Update nvim.aliases.bash

--- aliases/available/nvim.aliases.bash
+++ aliases/available/nvim.aliases.bash
@@ -1,1 +1,9 @@
+# shellcheck shell=bash
+about-alias 'nvim abbreviations'
 
+_command_exists nvim || return
+
+alias v='nvim'
+alias vim='nvim'
+# open the vim help in fullscreen incorporated from https://stackoverflow.com/a/4687513
+alias vimh='nvim -c ":h | only"'
